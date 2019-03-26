50742dd28c1bf03a3bb872adc289fe2b588ebf5b
Krzysztof Giblewski <krzysztofgiblewski@gmail.com>
2019-03-26 20:53:10 +0100

    Pierwszy po zrobieniu pierwszej lini

---
+++ src/sample/Controller.java
@@ -0,0 +1,53 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    private Label labelCreateTable;
+    @FXML
+    private Label labelNawiasPoczatek;
+    @FXML
+    private Label labelPrzecinek;
+    @FXML
+    private Label labelPrimaryKey;
+    @FXML
+    private Label labelNawiasKoniec;
+    @FXML
+    private Button generujButton;
+    @FXML
+    private TextField tekstFieldNazwaTabeli;
+    @FXML
+    private TextField tekstFieldNazwaKolumny;
+    @FXML
+    private TextField tekstFieldRozmiar;
+    @FXML
+    private TextField tekstFieldWartoscPoczatkowa;
+    @FXML
+    private TextField tekstFieldNotNull;
+    @FXML
+    private TextField tekstFieldAutoIncrement;
+    @FXML
+    private TextField tekstFieldNazwaPrimaryKey;
+    @FXML
+    private TextField tekstFieldWynikynik;
+    @FXML
+    private void generuj(){
+        tekstFieldWynikynik.setText(labelCreateTable.getText()
+                +" "+tekstFieldNazwaTabeli.getText()
+                +" "+labelNawiasPoczatek.getText()
+                +" "+tekstFieldNazwaKolumny.getText()
+                +" "+tekstFieldRozmiar.getText()
+                +" "+tekstFieldWartoscPoczatkowa.getText()
+                +" "+tekstFieldNotNull.getText()
+                +" "+tekstFieldAutoIncrement.getText()
+                +" "+labelPrzecinek.getText()+" "
+                +" "+labelPrimaryKey.getText()+" "
+                +" "+tekstFieldNazwaPrimaryKey.getText()+" "
+                +" "+labelNawiasKoniec.getText());
+    }
+
+}
